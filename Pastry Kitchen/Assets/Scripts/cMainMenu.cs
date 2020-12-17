e0de42ffc048497fa2a35a99ecc801ba3c15010f
r1ckl3ss <rick.herrera@g.austincc.edu>
2020-12-17 16:35:34 -0600

    Added Options and Setup Screen

---
+++ Pastry Kitchen/Assets/Scripts/cMainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cMainMenu : MonoBehaviour
+{
+    public Button bStart;
+    public Button bLoadGame;
+    public Button bOptions;
+    public Button bAbout;
+
+    public GameObject pOptions;
+
+    GameManager gm;
+    cOptions c_Options;
+
+    private void Start()
+    {
+        gm = GameManager.gm;
+    }
+
+    public void OnStartGameClicked()
+    {
+        gm.LoadScene(1);
+    }
+
+    public void OnOptionsClicked()
+    {
+        c_Options = Instantiate(pOptions).GetComponent<cOptions>();
+    }
+
+    public void OnExitGameClicked()
+    {
+        gm.ExitGame();
+    }
+
+}
---
+++ Pastry Kitchen/Assets/Scripts/cOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cOptions : MonoBehaviour
+{
+    public Button bBack;
+
+    public void OnBackClicked()
+    {
+        Destroy(gameObject);
+    }
+}
