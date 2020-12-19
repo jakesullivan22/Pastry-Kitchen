dafc672476a2beccc1bd64f85253634caed9d168
ROSA-PC\rosew1254 <jingyou.wang@g.austincc.edu>
2020-12-19 17:43:47 -0600

    Added prefabs for firewood and tray, script for distance check

---
+++ Pastry Kitchen/Assets/Scripts/sAccessory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sAccessory : MonoBehaviour
+{
+    public GameObject container, cloth, bowl, tray;
+
+    public void pickupAccessory()
+    {
+
+    }
+}
---
+++ Pastry Kitchen/Assets/Scripts/sInteractiveObj.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class sInteractiveObj : MonoBehaviour
+{
+    //List<GameObject> interObjList;
+    [NamedArray(typeof(eInteractiveObj))] public GameObject[] interObj;
+
+    private void Update()
+    {
+        float curDis;
+        curDis = Vector3.Distance(transform.position, getClosestInterObj().transform.position);
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log(getClosestInterObj());
+        }
+    }
+
+    public GameObject getClosestInterObj()
+    {
+        GameObject closestObj = null;
+        float closestDis = float.PositiveInfinity;
+        float curDis;
+        foreach(GameObject i in interObj)
+        {
+            curDis = Vector3.Distance(transform.position, i.transform.position);
+            if (curDis < closestDis)
+            {
+                closestObj = i;
+                closestDis = curDis;
+            }
+        }
+        return closestObj;
+    }
+}
